d4b777f64e3f666ee28ce15f7e389573ca96a810
Ingryd-julia <163544007+Ingryd-julia@users.noreply.github.com>
2024-04-05 02:08:03 -0300

    implementação do codigo do produto

---
+++ src/main/java/br/edu/iftm/ds/exemplos/arquitetura/control/Autwired.java
@@ -0,0 +1,5 @@
+package br.edu.iftm.ds.exemplos.arquitetura.control;
+
+public @interface Autwired {
+
+}
--- src/main/java/br/edu/iftm/ds/exemplos/arquitetura/control/ProdutoControl.java
+++ src/main/java/br/edu/iftm/ds/exemplos/arquitetura/control/ProdutoControl.java
@@ -3,17 +3,17 @@ package br.edu.iftm.ds.exemplos.arquitetura.control;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.kafka.KafkaProperties.Producer;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.edu.iftm.ds.exemplos.arquitetura.dao.protudodao;
 import br.edu.iftm.ds.exemplos.arquitetura.domain.produto;
 
 @RestController
 public class ProdutoControl {
     
     @Autowired
-    private Producer dao;
+    private protudodao dao;
 
     @GetMapping("/produto")
     public List<produto> getProdutos(){
